1517b0a6fa0ccbdd982a2b893f7fa24241f7683c
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-09-07 23:09:04 +0900

    add under construction message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import styles from './home.module.scss'
 import IntroduceMyself from '@/components/home/IntroduceMyself'
 import HyperLinkGroup from '@/components/home/HyperLinkGroup'
 import ThanksBanner from '@/components/home/ThanksBanner'
+import UnderConstruction from '@/components/home/UnderConstruction'
 
 export default function Home() {
   return (
     <main className={styles['main-container']}>
       <WelcomeText />
       <IntroduceMyself />
+      <UnderConstruction />
       <HyperLinkGroup />
       <span className={styles.break} />
       <ThanksBanner />
---
+++ src/components/home/UnderConstruction/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.scss'
+
+export default function UnderConstruction() {
+  return <p className={styles.text}>This website is under construction!</p>
+}

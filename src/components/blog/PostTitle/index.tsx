e981873e6da777baac92fbbabd2e2a8f821c88b6
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-09-08 21:46:22 +0900

    [wip] change title style

--- src/components/blog/PostTitle/index.tsx
+++ src/components/blog/PostTitle/index.tsx
@@ -5,9 +5,7 @@ type PostTitleProps = { children: string }
 export default function PostTitle({ children }: PostTitleProps) {
   return (
     <div className={styles.container}>
-      <div className={styles['text-container']}>
-        <h1>{children}</h1>
-      </div>
+      <h1>{children}</h1>
     </div>
   )
 }

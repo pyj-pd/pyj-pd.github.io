d53b825a5d23bab1252ccb07cb593117837e337c
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-09-10 20:12:17 +0900

    add nextjs option for export

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 import createMDX from '@next/mdx'
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',
   distDir: 'out',
   transpilePackages: ['next-mdx-remote'],
 }

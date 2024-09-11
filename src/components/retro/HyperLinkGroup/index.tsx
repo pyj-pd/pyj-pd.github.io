acdd5cc09c0ac10dd9b6d8489efaedcf3e13cbce
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-09-11 18:55:45 +0900

    change button to blog on main

--- src/components/retro/HyperLinkGroup/index.tsx
+++ src/components/retro/HyperLinkGroup/index.tsx
@@ -1,11 +1,10 @@
-import { GITHUB_PROFILE_LINK } from '@/constants/urls'
 import HyperLinkButton from './HyperLinkButton'
 import styles from './styles.module.scss'
 
 export default function HyperLinkGroup() {
   return (
     <div className={styles.container}>
-      <HyperLinkButton url={GITHUB_PROFILE_LINK}>GitHub</HyperLinkButton>
+      <HyperLinkButton url="/posts">Blog(한국어/Korean)</HyperLinkButton>
     </div>
   )
 }

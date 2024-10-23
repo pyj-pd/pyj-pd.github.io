c1c3dbeeafef2a2b67ab13ba94c0f931683d3d0b
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2024-10-23 21:02:46 +0900

    update to next.js 15. add eslint flat config.

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import prettier from 'eslint-plugin-prettier'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...compat.extends(
+    'next/core-web-vitals',
+    'prettier',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ),
+  {
+    plugins: {
+      prettier,
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+]

5437c7a7ddff605ac8b533b599e03ea5798a3622
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2025-02-12 00:33:26 +0900

    add paddings to sections in homepage

--- src/components/home/ProjectSection/index.tsx
+++ src/components/home/ProjectSection/index.tsx
@@ -8,23 +8,25 @@ export default function ProjectSection() {
   return (
     <section className={styles.container}>
       <PageTitle headingType="h2">프로젝트</PageTitle>
-      <p>스크린샷 이미지를 클릭하여 해당 프로젝트에 대해 더 알아보세요.</p>
-      <ul className={styles['project-container']}>
-        {projectList.map((projectItem, index) => (
-          <li key={index}>
-            <MyLink
-              href={`/projects#${projectItem.id}`}
-              className={styles['project-image-link']}
-            >
-              <Image
-                src={projectItem.image}
-                alt={`${projectItem.title} 프로젝트 스크린샷`}
-                height={200}
-              />
-            </MyLink>
-          </li>
-        ))}
-      </ul>
+      <div className={styles['content-container']}>
+        <p>스크린샷 이미지를 클릭하여 해당 프로젝트에 대해 더 알아보세요.</p>
+        <ul className={styles['project-container']}>
+          {projectList.map((projectItem, index) => (
+            <li key={index}>
+              <MyLink
+                href={`/projects#${projectItem.id}`}
+                className={styles['project-image-link']}
+              >
+                <Image
+                  src={projectItem.image}
+                  alt={`${projectItem.title} 프로젝트 스크린샷`}
+                  height={200}
+                />
+              </MyLink>
+            </li>
+          ))}
+        </ul>
+      </div>
     </section>
   )
 }

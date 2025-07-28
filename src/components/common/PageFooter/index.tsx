2444b0f404327d3df6edbdd9b0077b1ff3f78b9e
Yu Jun Park <123401626+pyj-pd@users.noreply.github.com>
2025-07-28 12:44:38 +0900

    add external links to footer

--- src/components/common/PageFooter/index.tsx
+++ src/components/common/PageFooter/index.tsx
@@ -1,15 +1,31 @@
+import { externalRoutesList } from '@/constants/routes'
 import styles from './styles.module.scss'
+
+const externalRouteIds = Object.keys(
+  externalRoutesList,
+) as (keyof typeof externalRoutesList)[]
 
 export default function PageFooter() {
   return (
     <footer className={styles.container}>
-      <p>End of page</p>
-      <a
-        href="#"
-        className={styles['go-to-top']}
-      >
-        맨 위로
-      </a>
+      <div>
+        <p>END OF PAGE</p>
+        <a href="#">맨 위로</a>
+      </div>
+      <div>
+        {externalRouteIds.map((routeId) => {
+          const routeData = externalRoutesList[routeId]
+
+          return (
+            <a
+              key={routeId}
+              href={routeData.path}
+            >
+              {routeData.name}
+            </a>
+          )
+        })}
+      </div>
     </footer>
   )
 }
